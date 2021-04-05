e3d61b935f1e81a9c37c6ece672aa9e434d90a25
HaiderRizvii <sha.rizvi883@gmail.com>
2021-04-06 04:59:04 +0500

    Server added

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+    res
+        .status(200)
+        .send("Server started successfully")
+
+})
+
+
+
+module.exports = app;
